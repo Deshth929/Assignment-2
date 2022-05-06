6f98a02b03c3ad7cde065f0a91e7a76d2afc8598
Deshth929 <105069840+Deshth929@users.noreply.github.com>
2022-05-06 22:40:21 +0530

    Update python assignment 2 question 5.py

--- python assignment 2 question 5.py
+++ python assignment 2 question 5.py
@@ -1,12 +1,12 @@
-a=input("Enter the side 1 of triangle")
-b=input("Enter the side 2 of triangle")
-c=input("Enter the side 3 of triangle")
+a=int(input("Enter the side 1 of triangle"))
+b=int(input("Enter the side 2 of triangle"))
+c=int(input("Enter the side 3 of triangle"))
 sum1=a+b
 sum2=b+c
 sum3=c+a
-d=(a<=sum1)
-i=(b<=sum2)
-t=(c<=sum3)
+d=(c<=sum1)
+i=(a<=sum2)
+t=(b<=sum3)
 print(d and i and t )
 
 
